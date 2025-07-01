94cbeaa817dddad73f70fd6757d950790941bb99
Tomasini Luca <luca.tomasini@heig-vd.ch>
2025-07-01 09:07:47 +0200

    Add combined model

---
+++ src/optimization_model/combined_model/variables.py
@@ -0,0 +1,19 @@
+
+
+import pyomo.environ as pyo
+
+def model_variables(model: pyo.AbstractModel) -> pyo.AbstractModel:
+    
+    # Candidate-indexed branch variables.
+    model.d = pyo.Var(model.LC, domain=pyo.Binary)
+    model.delta = pyo.Var(model.S, domain=pyo.Binary)
+    
+    model.p_flow = pyo.Var(model.LC, domain=pyo.Reals)
+    model.q_flow = pyo.Var(model.LC, domain=pyo.Reals)
+    model.i_sq = pyo.Var(model.LC, domain=pyo.NonNegativeReals)
+    model.v_sq = pyo.Var(model.N, domain=pyo.NonNegativeReals) 
+    model.slack_v_pos = pyo.Var(model.N, domain=pyo.NonNegativeReals)
+    model.slack_v_neg = pyo.Var(model.N, domain=pyo.NonNegativeReals)
+    model.slack_i_sq = pyo.Var(model.LC, domain=pyo.NonNegativeReals)
+    return model
+
