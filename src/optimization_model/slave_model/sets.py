0aa8d2725805a99eb6c5a2ce1aeffd2bd1b17ae9
mohammadrayati <mohammad.rayiati@gmail.com>
2025-07-11 11:15:50 +0200

    refactor: remove redundant model.C_2 set definition in slave_model_sets

--- src/optimization_model/slave_model/sets.py
+++ src/optimization_model/slave_model/sets.py
@@ -6,7 +6,6 @@ def slave_model_sets(model: pyo.AbstractModel) -> pyo.AbstractModel:
     model.L = pyo.Set()  # Edges indices.
     model.S = pyo.Set(within=model.L)  # Switch indices
     model.C = pyo.Set(dimen=3, within=model.L * model.N * model.N)  # type: ignore
-    model.C_2 = pyo.Set(dimen=3, within=model.L * model.N * model.N)  # type: ignore
     model.nS = pyo.Set(initialize=lambda m: [l for l in m.L if l not in m.S])
 
     return model
