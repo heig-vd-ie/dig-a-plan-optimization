e81c82c89ee316a2dc54c6ed024e989b8cc424c3
mohammadrayati <mohammad.rayiati@gmail.com>
2025-07-25 11:36:45 +0200

    refactor: reorganize grid data handling and remove unused imports

--- examples/03-combined_model_boisy.py
+++ examples/03-combined_model_boisy.py
@@ -1,17 +1,16 @@
 # %% import libraries
 import os
-import networkx as nx
+import pandapower as pp
 from polars import col as c
-from networkx import connected_components
 import polars as pl
 from local_data_exporter import (
     change_schema_to_dig_a_plan_schema,
     duckdb_to_changes_schema,
 )
+from local_data_exporter import pandapower_to_dig_a_plan_schema
 
 from pipelines import DigAPlan
 from pipelines.configs import CombinedConfig, PipelineType
-from networkx_function import generate_nx_edge, get_connected_edges_data
 
 
 # ensure working directory is project root
@@ -19,16 +18,21 @@ os.chdir(os.getcwd().replace("/src", ""))
 os.environ["GRB_LICENSE_FILE"] = os.environ["HOME"] + "/gurobi_license/gurobi.lic"
 
 # %% set parameters
-change_schema = duckdb_to_changes_schema(".cache/boisy_grid.db")
+if USE_SIMPLIFIED_GRID := True:
+    net = pp.from_pickle(".cache/boisy_grid_simplified.p")
+    base_grid_data = pandapower_to_dig_a_plan_schema(net)
+else:
+    change_schema = duckdb_to_changes_schema(".cache/boisy_grid.db")
+    base_grid_data = change_schema_to_dig_a_plan_schema(change_schema, 1000)
 
-base_grid_data = change_schema_to_dig_a_plan_schema(change_schema, 1000)
+# %% convert pandapower grid to DigAPlan grid data
 base_grid_data.node_data = base_grid_data.node_data.with_columns(
     pl.lit(0.01).alias("p_node_pu"),
     pl.lit(0.01).alias("q_node_pu"),
 )
 base_grid_data.edge_data = base_grid_data.edge_data.with_columns(
-    # pl.lit(0.001).alias("r_pu"),
-    # pl.lit(0.001).alias("x_pu"),
+    pl.lit(0.001).alias("r_pu"),
+    pl.lit(0.001).alias("x_pu"),
     pl.lit(0).alias("b_pu"),
 )
 base_grid_data.node_data = base_grid_data.node_data.with_columns(
