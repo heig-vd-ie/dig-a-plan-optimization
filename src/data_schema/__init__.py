abb3e63aff1087c9728cba54264ab0f9c6c5725e
mohammadrayati <mohammad.rayiati@gmail.com>
2025-07-29 08:23:10 +0200

    refactor: update NodeEdgeModel to use pt.DataFrame with default factories for node and edge data

--- src/data_schema/__init__.py
+++ src/data_schema/__init__.py
@@ -1,11 +1,16 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+import patito as pt
 from edge_data import EdgeData
 from node_data import NodeData
 from load_data import LoadData
 
 
-@dataclass
+@dataclass(frozen=True)
 class NodeEdgeModel:
-    node_data: NodeData
-    edge_data: EdgeData
-    load_data: dict[str, LoadData]
+    node_data: pt.DataFrame = field(
+        default_factory=lambda: NodeData.DataFrame(schema=NodeData.columns).cast()
+    )
+    edge_data: pt.DataFrame = field(
+        default_factory=lambda: EdgeData.DataFrame(schema=EdgeData.columns).cast()
+    )
+    load_data: dict[str, pt.DataFrame] = field(default_factory=dict)
