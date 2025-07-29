911de60740e7fcd13d6a78bb775f52d12f02ecb5
mohammadrayati <mohammad.rayiati@gmail.com>
2025-07-29 08:27:12 +0200

    refactor: update variable names in constraints for consistency and clarity

--- src/optimization_model/constraints.py
+++ src/optimization_model/constraints.py
@@ -20,12 +20,12 @@ def objective_rule_infeasibility(m):
 
 
 def objective_rule_penalty(m):
-    return sum(m.delta_penalty[l] for l in m.S)
+    return sum(m.δ_penalty[l] for l in m.S)
 
 
 def objective_rule_admm_penalty(m):
-    return sum(m.u_param[s] * m.delta[s] for s in m.S) + (m.rho / 2.0) * sum(
-        (m.delta[s] - m.del_param[s]) ** 2 for s in m.S
+    return sum(m.λ[s] * m.δ[s] for s in m.S) + (m.ρ / 2.0) * sum(
+        (m.δ[s] - m.z[s]) ** 2 for s in m.S
     )
 
 
@@ -35,6 +35,7 @@ def objective_rule_combined(m):
         objective_rule_loss(m)
         + objective_rule_infeasibility(m) * m.weight_infeasibility
         + objective_rule_penalty(m) * m.weight_penalty
+        + objective_rule_admm_penalty(m) * m.weight_admm_penalty
     )
 
 
@@ -58,20 +59,20 @@ def imaginary_flow_edge_propagation_rule(m, l):
 
 def imaginary_flow_upper_switch_propagation_rule(m, l, i, j):
     if l in m.S:
-        return m.flow[l, i, j] <= m.small_m * len(m.N) * m.delta[l]
+        return m.flow[l, i, j] <= m.small_m * len(m.N) * m.δ[l]
     else:
         return pyo.Constraint.Skip
 
 
 def imaginary_flow_lower_switch_propagation_rule(m, l, i, j):
     if l in m.S:
-        return m.flow[l, i, j] >= -m.small_m * len(m.N) * m.delta[l]
+        return m.flow[l, i, j] >= -m.small_m * len(m.N) * m.δ[l]
     else:
         return pyo.Constraint.Skip
 
 
 def imaginary_flow_nb_closed_switches_rule(m):
-    return sum(m.delta[l] for l in m.S) == len(m.N) - len(m.nS) - 1
+    return sum(m.δ[l] for l in m.S) == len(m.N) - len(m.nS) - 1
 
 
 def radiality_rule(m, n):
@@ -83,13 +84,13 @@ def radiality_rule(m, n):
 
 def edge_direction_rule(m, l):
     if l in m.S:
-        return sum(m.d[l_, i, j] for l_, i, j in m.C if l_ == l) == m.delta[l]
+        return sum(m.d[l_, i, j] for l_, i, j in m.C if l_ == l) == m.δ[l]
     else:
         return pyo.Constraint.Skip
 
 
 def master_switch_status_propagation_rule(m, s):
-    return m.delta[s] == m.master_delta[s]
+    return m.δ[s] == m.master_δ[s]
 
 
 # (1) Slack Bus: fix bus 0's voltage squared to 1.0.
@@ -198,14 +199,14 @@ def current_rotated_cone_rule(m, l, i, j):
 
 def voltage_drop_upper_rule(m, l, i, j):
     if l in m.S:
-        return m.v_sq[i] - m.v_sq[j] <= m.big_m * (1 - m.delta[l])
+        return m.v_sq[i] - m.v_sq[j] <= m.big_m * (1 - m.δ[l])
     else:
         return pyo.Constraint.Skip
 
 
 def voltage_drop_lower_rule(m, l, i, j):
     if l in m.S:
-        return m.v_sq[i] - m.v_sq[j] >= -m.big_m * (1 - m.delta[l])
+        return m.v_sq[i] - m.v_sq[j] >= -m.big_m * (1 - m.δ[l])
     else:
         dv = (
             -2 * (m.r[l] * m.p_flow[l, i, j] + m.x[l] * m.q_flow[l, i, j])
@@ -221,14 +222,14 @@ def voltage_drop_lower_rule(m, l, i, j):
 
 def voltage_drop_upper_lindistflow_rule(m, l, i, j):
     if l in m.S:
-        return m.v_sq[i] - m.v_sq[j] <= m.big_m * (1 - m.delta[l])
+        return m.v_sq[i] - m.v_sq[j] <= m.big_m * (1 - m.δ[l])
     else:
         return pyo.Constraint.Skip
 
 
 def voltage_drop_lower_lindistflow_rule(m, l, i, j):
     if l in m.S:
-        return m.v_sq[i] - m.v_sq[j] >= -m.big_m * (1 - m.delta[l])
+        return m.v_sq[i] - m.v_sq[j] >= -m.big_m * (1 - m.δ[l])
     else:
         dv = -2 * (m.r[l] * m.p_flow[l, i, j] + m.x[l] * m.q_flow[l, i, j])
         voltage_diff = (
@@ -241,28 +242,28 @@ def voltage_drop_lower_lindistflow_rule(m, l, i, j):
 
 def switch_active_power_lower_bound_rule(m, l, i, j):
     if l in m.S:
-        return m.p_flow[l, i, j] >= -m.big_m * m.delta[l]
+        return m.p_flow[l, i, j] >= -m.big_m * m.δ[l]
     else:
         return pyo.Constraint.Skip
 
 
 def switch_active_power_upper_bound_rule(m, l, i, j):
     if l in m.S:
-        return m.p_flow[l, i, j] <= m.big_m * m.delta[l]
+        return m.p_flow[l, i, j] <= m.big_m * m.δ[l]
     else:
         return pyo.Constraint.Skip
 
 
 def switch_reactive_power_lower_bound_rule(m, l, i, j):
     if l in m.S:
-        return m.q_flow[l, i, j] >= -m.big_m * m.delta[l]
+        return m.q_flow[l, i, j] >= -m.big_m * m.δ[l]
     else:
         return pyo.Constraint.Skip
 
 
 def switch_reactive_power_upper_bound_rule(m, l, i, j):
     if l in m.S:
-        return m.q_flow[l, i, j] <= m.big_m * m.delta[l]
+        return m.q_flow[l, i, j] <= m.big_m * m.δ[l]
     else:
         return pyo.Constraint.Skip
 
