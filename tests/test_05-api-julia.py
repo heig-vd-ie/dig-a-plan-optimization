a66a939d91cfa84bd026fbb6634e8afcd0d891cc
mohammadrayati <mohammad.rayiati@gmail.com>
2025-08-08 17:40:19 +0200

    tests: enhance expansion model test with detailed assertions

--- tests/test_05-api-julia.py
+++ tests/test_05-api-julia.py
@@ -1,8 +1,18 @@
 from pipelines.expansion.api import ExpansionModel
+import numpy as np
+import math
 
 
 def test_expansion_model():
     expansion_model = ExpansionModel()
-    results = expansion_model.run_sddp()
-    assert results is not None
-    assert results.status_code == 200
+    response = expansion_model.run_sddp()
+    assert response is not None
+    assert response.status_code == 200
+    assert math.isclose(
+        np.mean(response.json()["objectives"]), 4298.449745512054, abs_tol=1e-5
+    )
+    assert math.isclose(
+        np.std(response.json()["objectives"]), 3248.973223357565, abs_tol=1e-5
+    )
+    assert response.json()["simulations"] is not None
+    assert len(response.json()["simulations"]) == 100
