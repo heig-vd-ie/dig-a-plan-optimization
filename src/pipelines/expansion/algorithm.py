ee47896dba4f41a69ad94f480bf764478354c122
mohammadrayati <mohammad.rayiati@gmail.com>
2025-08-25 16:03:18 +0200

    fix: adjust random simulation index to prevent out-of-bounds error in heavy_task function

--- src/pipelines/expansion/algorithm.py
+++ src/pipelines/expansion/algorithm.py
@@ -304,7 +304,7 @@ def heavy_task(
 
     os.environ["GRB_LICENSE_FILE"] = os.environ["HOME"] + "/gurobi_license/gurobi.lic"
 
-    rand_ω = random.randint(0, n_simulations)
+    rand_ω = random.randint(0, n_simulations - 1)
     admm.update_grid_data(
         δ_load=sddp_response.simulations[rand_ω][stage].δ_load,
         δ_pv=sddp_response.simulations[rand_ω][stage].δ_pv,
