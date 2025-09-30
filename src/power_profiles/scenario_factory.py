6c51bb1f68b6c2135646fed960401c3c961e600e
Mohammad Rayati <mohammad.rayiati@gmail.com>
2025-09-30 17:29:01 +0200

    feat: Refactor ScenarioFactory to utilize LoadType enum and improve get_scenario method

--- src/power_profiles/scenario_factory.py
+++ src/power_profiles/scenario_factory.py
@@ -3,6 +3,15 @@ import pandapower as pp
 from general_function import duckdb_to_dict
 from power_profiles.download_from_swisstopo import download_estavayer_power_profiles
 from config import settings
+from enum import Enum
+
+
+class LoadType(Enum):
+    PV = "PV"
+    EV = "EV"
+    HP = "HP"
+    DHW = "DHW"
+    TOTAL = "TOTAL"
 
 
 class ScenarioFactory:
@@ -11,13 +20,20 @@ class ScenarioFactory:
         download_estavayer_power_profiles(kace=kace, force=False)
         self.kace = kace
 
-    def get_scenario(self):
-        pv_df = pl.read_parquet(
-            f"{settings.cases[self.kace].load_allocation_folder}/PV.parquet"
+    def _read_parquet(self, file_path: str) -> pl.DataFrame:
+        return pl.read_parquet(
+            f"{settings.cases[self.kace].load_allocation_folder}/{file_path}.parquet"
         )
+
+    def get_scenario(self):
+        dfs = {k.value: self._read_parquet(k.value) for k in LoadType}
         load_duckdb = duckdb_to_dict(settings.cases[self.kace].load_duckdb_file)
         net = pp.from_pickle(settings.cases[self.kace].pandapower_file)
-        return pv_df, load_duckdb, net
+        return (
+            dfs,
+            load_duckdb,
+            net,
+        )
 
 
 if __name__ == "__main__":
