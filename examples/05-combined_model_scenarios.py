5c11e4742125683cd226d0a48086ffb38f6d169f
Mohammad Rayati <mohammad.rayiati@gmail.com>
2025-10-10 10:23:26 +0200

    fix: update PYTHONPATH in .envrc and comment out plotting and comparison sections in 05-combined_model_scenarios.py

--- examples/05-combined_model_scenarios.py
+++ examples/05-combined_model_scenarios.py
@@ -21,6 +21,7 @@ config = CombinedConfig(
     γ_infeasibility=100.0,
     γ_admm_penalty=0.0,
     all_scenarios=True,
+    time_limit=1800,
 )
 dig_a_plan = DigAPlanCombined(config=config)
 
@@ -37,11 +38,11 @@ voltages = dig_a_plan.result_manager.extract_node_voltage()
 currents = dig_a_plan.result_manager.extract_edge_current()
 
 
-# %% plot the grid annotated with DigAPlan results
-fig = plot_grid_from_pandapower(net=net, dap=dig_a_plan)
+# # %% plot the grid annotated with DigAPlan results
+# fig = plot_grid_from_pandapower(net=net, dap=dig_a_plan)
 
-# %% compare DigAPlan results with pandapower results
+# # %% compare DigAPlan results with pandapower results
 
-node_data, edge_data = compare_dig_a_plan_with_pandapower(
-    dig_a_plan=dig_a_plan, net=net
-)
+# node_data, edge_data = compare_dig_a_plan_with_pandapower(
+#     dig_a_plan=dig_a_plan, net=net
+# )
