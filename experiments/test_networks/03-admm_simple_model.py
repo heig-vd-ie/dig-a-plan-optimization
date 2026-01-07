f954f45fd1975bb4e9ce92616059976fbd87f0c2
adeleh1379 <adelehmg@gmail.com>
2026-01-07 12:22:57 +0100

    refactor: load grid data via API for improved modularity and clarity

--- experiments/test_networks/03-admm_simple_model.py
+++ experiments/test_networks/03-admm_simple_model.py
@@ -1,12 +1,22 @@
 # %%
 from experiments import *
 from pathlib import Path
+from api.grid_cases import get_grid_case
+from data_model.kace import GridCaseModel
+from data_model.reconfiguration import ShortTermUncertainty
+
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 
-# %% set parameters
+# %% --- Load net via API  ---
+grid = GridCaseModel(
+    pp_file=str(PROJECT_ROOT / "examples" / "ieee-33" / "simple_grid.p"),
+    s_base=1e6,
+)
+stu = ShortTermUncertainty()
 
-net = pp.from_pickle(str(PROJECT_ROOT / "examples/ieee-33/simple_grid.p"))
+net, _ = get_grid_case(grid=grid, seed=42, stu=stu)
+# %% --- build grid data with scenarios ---
 net.bus["max_vm_pu"] = 1.05
 net.bus["min_vm_pu"] = 0.95
 grid_data = pandapower_to_dig_a_plan_schema_with_scenarios(
