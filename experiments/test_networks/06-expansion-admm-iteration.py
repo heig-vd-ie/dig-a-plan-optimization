fa8626ae7d1e8a281f99c0fa6584f2b0e6d90357
adeleh1379 <adelehmg@gmail.com>
2026-01-07 16:55:41 +0100

    refactor: reorganize code to load grid data via API and enhance parameter settings

--- experiments/test_networks/06-expansion-admm-iteration.py
+++ experiments/test_networks/06-expansion-admm-iteration.py
@@ -1,19 +1,28 @@
-# %%
-import os
+# %% import libraries
 
-# %%
 from experiments import *
 from pathlib import Path
+from api.grid_cases import get_grid_case
+from data_model.kace import GridCaseModel
+from data_model.reconfiguration import ShortTermUncertainty
+
 PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
-# %% set parameters
 
-net = pp.from_pickle(str(PROJECT_ROOT / "examples/ieee-33/simple_grid.p"))
+# %% --- Load net via API  ---
+grid = GridCaseModel(
+    pp_file=str(PROJECT_ROOT / "examples" / "ieee-33" / "simple_grid.p"),
+    s_base=1e6,
+)
+stu = ShortTermUncertainty()
+
+net, _ = get_grid_case(grid=grid, seed=42, stu=stu)
+# %% --- build grid data with scenarios ---
 net.bus["max_vm_pu"] = 1.05
 net.bus["min_vm_pu"] = 0.95
 grid_data = pandapower_to_dig_a_plan_schema_with_scenarios(
     net,
-    number_of_random_scenarios=10,
+    number_of_random_scenarios=100,
     p_bounds=(-0.6, 1.5),
     q_bounds=(-0.1, 0.1),
     v_bounds=(-0.1, 0.1),
