3a5b3f0d99af02e66db8e62c239dacdd252688c8
Mohammad Rayati <mohammad.rayiati@gmail.com>
2026-01-07 11:29:21 +0100

    fix: use ShortTermUncertaintyRandom instead of ShortTermUncertainty in experiments

--- experiments/test_networks/01-bender_simple_grid.py
+++ experiments/test_networks/01-bender_simple_grid.py
@@ -2,27 +2,31 @@
 from experiments import *
 from api.grid_cases import get_grid_case
 from data_model.kace import GridCaseModel
-from data_model.reconfiguration import ShortTermUncertainty
+from data_model.reconfiguration import ShortTermUncertaintyRandom
 from pathlib import Path
-PROJECT_ROOT = Path(__file__).resolve().parents[2]  
+
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
 
 
-grid = GridCaseModel(pp_file=str(PROJECT_ROOT / "examples" / "ieee-33" / "simple_grid.p"), s_base=1e6)
-stu = ShortTermUncertainty(
-    number_of_scenarios=10,
+grid = GridCaseModel(
+    pp_file=str(PROJECT_ROOT / "examples" / "ieee-33" / "simple_grid.p"), s_base=1e6
+)
+stu = ShortTermUncertaintyRandom(
+    n_scenarios=10,
     p_bounds=(-0.2, 0.2),
     q_bounds=(-0.2, 0.2),
     v_bounds=(-0.03, 0.03),
 )
 
 
-# Use API to load the net 
+# Use API to load the net
 net, _ = get_grid_case(grid=grid, seed=42, stu=stu)
 
 # %% build base_grid_data
-base_grid_data = pandapower_to_dig_a_plan_schema_with_scenarios(net=net,
+base_grid_data = pandapower_to_dig_a_plan_schema_with_scenarios(
+    net=net,
     s_base=grid.s_base,
-    number_of_random_scenarios=stu.number_of_scenarios,
+    number_of_random_scenarios=stu.n_scenarios,
     p_bounds=stu.p_bounds,
     q_bounds=stu.q_bounds,
     v_bounds=stu.v_bounds,
