2df7641bd04508744e46f753660f7fc050feda53
Mohammad Rayati <mohammad.rayiati@gmail.com>
2026-01-14 14:51:08 +0100

    refactor: reorganize debug code for local testing in ADMM script

--- experiments/boisy/01-admm.py
+++ experiments/boisy/01-admm.py
@@ -5,14 +5,16 @@ if __name__ == "__main__":
     payload = json.load(open(payload_file, "r"))
     request = ADMMInput.model_validate(payload)
 
-    ## TO DEBUG LOCALLY WITHOUT API CALL ##
-    # from api.admm import run_admm
+    if False:
+        ## TO DEBUG LOCALLY WITHOUT API CALL ##
+        from api.admm import run_admm
 
-    # results = run_admm(request)
-    ######################################
-    response = requests.patch(
-        f"http://{LOCALHOST}:{PY_PORT}/reconfiguration/admm",
-        json=payload,
-    )
-    print("Response status code:", response.status_code)
-    print("Response JSON:", response.json())
+        results = run_admm(request)
+        ######################################
+    else:
+        response = requests.patch(
+            f"http://{LOCALHOST}:{PY_PORT}/reconfiguration/admm",
+            json=payload,
+        )
+        print("Response status code:", response.status_code)
+        print("Response JSON:", response.json())
