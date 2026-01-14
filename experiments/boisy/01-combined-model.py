3897be62a4d7a84164b3482346ce5d39cdf8436d
Mohammad Rayati <mohammad.rayiati@gmail.com>
2026-01-14 13:03:48 +0100

    refactor: streamline local debugging setup in boisy experiment script

--- experiments/boisy/01-combined-model.py
+++ experiments/boisy/01-combined-model.py
@@ -6,15 +6,15 @@ if __name__ == "__main__":
     request = CombinedInput.model_validate(payload)
 
     ## TO DEBUG LOCALLY WITHOUT API CALL ##
-    from api.combined import run_combined
+    # from api.combined import run_combined
 
-    request.profiles = None  # to speed up local testing
-    results = run_combined(request)
+    # request.profiles = None  # to speed up local testing
+    # results = run_combined(request)
     ######################################
-    # payload["profiles"] = None
-    # response = requests.patch(
-    #     f"http://{LOCALHOST}:{PY_PORT}/reconfiguration/combined",
-    #     json=payload,
-    # )
-    # print("Response status code:", response.status_code)
-    # print("Response JSON:", response.json())
+    payload["profiles"] = None
+    response = requests.patch(
+        f"http://{LOCALHOST}:{PY_PORT}/reconfiguration/combined",
+        json=payload,
+    )
+    print("Response status code:", response.status_code)
+    print("Response JSON:", response.json())
