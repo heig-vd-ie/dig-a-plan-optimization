c6676f09ef6065d1a24e8be49ef7f78060664a11
Mohammad Rayati <mohammad.rayiati@gmail.com>
2026-02-20 10:23:15 +0100

    fix: add normal optimization into boisy feeders

--- scripts/run-all.sh
+++ scripts/run-all.sh
@@ -29,12 +29,20 @@ if [ "$FIRST_ONLY" = true ]; then
 fi
 
 # Additional experiments
-python experiments/expansion_planning_script.py --kace boisy --feedername feeder_1 --fixed_switches true --cachename boisy_feeder_1
-echo "Finished feeder_1"
+python experiments/expansion_planning_script.py --kace boisy --feedername feeder_1 --fixed_switches true --cachename boisy_feeder_1_fixed_switches
+echo "Finished feeder_1_fixed_switches"
 sleep $DELAY_TIME
-python experiments/expansion_planning_script.py --kace boisy --feedername feeder_2 --fixed_switches true --cachename boisy_feeder_2
-echo "Finished feeder_2"
+python experiments/expansion_planning_script.py --kace boisy --feedername feeder_1 --admmiter 3 --cachename boisy_feeder_1_expectation
+echo "Finished feeder_1_expectation"
 sleep $DELAY_TIME
+
+python experiments/expansion_planning_script.py --kace boisy --feedername feeder_2 --fixed_switches true --cachename boisy_feeder_2_fixed_switches
+echo "Finished feeder_2_fixed_switches"
+sleep $DELAY_TIME
+python experiments/expansion_planning_script.py --kace boisy --feedername feeder_2 --admmiter 3 --cachename boisy_feeder_2_expectation
+echo "Finished feeder_2_expectation"
+sleep $DELAY_TIME
+
 python experiments/expansion_planning_script.py --kace estavayer --feedername aumont --fixed_switches true --cachename estavayer_aumont
 echo "Finished aumont"
 sleep $DELAY_TIME
@@ -47,11 +55,11 @@ sleep $DELAY_TIME
 python experiments/expansion_planning_script.py --kace estavayer --feedername centre_ville --fixed_switches true --cachename estavayer_centre_ville
 echo "Finished centre_ville"
 sleep $DELAY_TIME
-python experiments/expansion_planning_script.py --kace estavayer --feedername st-aubin --fixed_switches true --cachename estavayer_st-aubin
-echo "Finished st-aubin"
-sleep $DELAY_TIME
 python experiments/expansion_planning_script.py --kace estavayer --feedername tout_vent --fixed_switches true --cachename estavayer_tout_vent
 echo "Finished tout_vent"
 sleep $DELAY_TIME
 python experiments/expansion_planning_script.py --kace estavayer --feedername zone_industrielle --fixed_switches true --cachename estavayer_zone_industrielle
 echo "Finished zone_industrielle"
+python experiments/expansion_planning_script.py --kace estavayer --feedername st-aubin --fixed_switches true --cachename estavayer_st-aubin
+echo "Finished st-aubin"
+sleep $DELAY_TIME
